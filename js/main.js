3d63524dd1e2f6c5ea851c7b26602f5dbe1f158e
ash-programmerheroine <ashrafikhanam.18@gmail.com>
2021-09-02 21:59:15 +0600

    2 section left..almost done

---
+++ js/main.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+    $("#option-1").addClass("active");
+    $("#content-one").show();
+    $("#content-two").hide();
+    $("#content-three").hide();
+
+    $("#option-1").click(function(){
+        //Option Tab Class active
+        $("#option-1").addClass("active");
+        $("#option-2").removeClass("active");
+        $("#option-3").removeClass("active");
+
+        //Option Tab Content Show
+        $("#content-one").show();
+        $("#content-two").hide();
+        $("#content-three").hide();
+
+    })
+    $("#option-2").click(function(){
+        //Option Tab Class active
+        $("#option-1").removeClass("active");
+        $("#option-2").addClass("active");
+        $("#option-3").removeClass("active");
+
+        //Option Tab Content Show
+        $("#content-one").hide();
+        $("#content-two").show();
+        $("#content-three").hide();
+
+    })
+    $("#option-3").click(function(){
+        //Option Tab Class active
+        $("#option-1").removeClass("active");
+        $("#option-2").removeClass("active");
+        $("#option-3").addClass("active");
+
+        //Option Tab Content Show
+        $("#content-one").hide();
+        $("#content-two").hide();
+        $("#content-three").show();
+
+    })
+
+});
